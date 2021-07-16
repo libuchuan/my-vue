f8230f101f89055383b48ad5712f78c076d0318d
zxm <1069406508@qq.com>
2021-07-16 20:49:46 +0800

    commit

---
+++ blog-vue/src/views/admin/users/Comments.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" align="center">
+
+    <!-- <h1>此功能尚在开发</h1> -->
+    <el-container>
+      <div class="main-pic" align="middle" style="margin:auto">
+        <!-- <img src="" class="main-pic" width="400" height="400" align="middle"> -->
+      </div>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comments'
+}
+</script>
+
+<style scoped>
+
+</style>
